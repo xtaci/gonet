0c16b56f3e43c5cb3dc8e7714cb02980bd45e1ae
xtaci <xtaci@ubuntu.(none)>
2013-04-12 09:39:56 +0800

    add uint and string escape for sql dump

--- src/db/sql.go
+++ src/db/sql.go
@@ -2,8 +2,11 @@ package db
 
 import "fmt"
 import "reflect"
+import "regexp"
 
-func sql_dump(tbl interface{})(fields []string, values []string){
+func sql_dump(tbl interface{})(fields []string, values []string) {
+	re := regexp.MustCompile(`(\'|\"|\.|\*|\/|\-|\\)`)
+
 	v := reflect.ValueOf(tbl).Elem()
 	key := v.Type()
 	count := key.NumField()
@@ -15,10 +18,15 @@ func sql_dump(tbl interface{})(fields []string, values []string){
 	for i := 0; i < count; i++ {
 		typeok := true
 		switch v.Field(i).Kind() {
-		case reflect.Int:
+		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
 			values[slice_idx] = fmt.Sprintf("'%d'",v.Field(i).Int())
+		case reflect.Uint,reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+			values[slice_idx] = fmt.Sprintf("'%d'",v.Field(i).Uint())
+		case reflect.Float32, reflect.Float64:
+			values[slice_idx] = fmt.Sprintf("'%f'", v.Field(i).Float())
 		case reflect.String:
-			values[slice_idx] = fmt.Sprintf("'%s'",v.Field(i).String())
+			tmpstr := re.ReplaceAllString(v.Field(i).String(), `\${1}`)
+			values[slice_idx] = fmt.Sprintf("'%s'",tmpstr)
 		default:
 			typeok = false
 		}
