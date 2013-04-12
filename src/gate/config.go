c07758baf6dd54a61390ff59de4850aba7794d6e
xtaci <xtaci@ubuntu.(none)>
2013-04-12 16:36:57 +0800

    add config load procedure

---
+++ src/gate/config.go
@@ -0,0 +1,37 @@
+package main
+
+import "os"
+import "io"
+import "bufio"
+import "regexp"
+import "fmt"
+
+func read_config(path string)(ret map[string]string) {
+	ret = make(map[string]string)
+	f, err := os.Open(path)
+
+	if err != nil {
+		fmt.Println("error opening file %v\n", err)
+		os.Exit(1)
+	}
+
+	re := regexp.MustCompile(`[\t ]*([0-9A-Za-z_]+)[\t ]*=[\t ]*([0-9A-Za-z_:?]+)[\t ]*`)
+
+	r := bufio.NewReader(f)
+	for {
+		line,e := r.ReadString('\n')
+
+		// maping
+		slice := re.FindStringSubmatch(line)
+
+		if slice != nil{
+			ret[slice[1]] = slice[2]
+		}
+
+		if e == io.EOF {
+			break
+		}
+	}
+
+	return
+}
---
+++ src/gate/config_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestReadConfig(t *testing.T) {
+	m := read_config("config.ini")
+
+	if m["service"] == "" {
+		t.Error("load config file failed")
+	}
+}
