ad1a0afd82edb27eee45207e731a7d5123cc8af9
xtaci <xtaci@ubuntu.(none)>
2013-04-12 12:21:04 +0800

    add a test

--- src/db/utils_test.go
+++ src/db/utils_test.go
@@ -3,9 +3,17 @@ package db
 import "testing"
 
 func TestCamelcase(t *testing.T) {
-	if camelcase("foo_bar") == "FooBar" && camelcase("_foo_bar") == "FooBar" {
-		t.Log("passed")
-	} else {
-		t.Error("failed")
+	if camelcase("foo_bar") != "FooBar" {
+		t.Error("failed foo_bar")
+	}
+
+	if camelcase("_foo_bar") != "FooBar" {
+		t.Error("failed _foo_bar")
+	}
+}
+
+func TestUnderscore(t *testing.T) {
+	if underscore("FooBar") != "foo_bar" {
+		t.Error("failed FooBar")
 	}
 }
