92b2a72ad9092a389c72006cfbe2d4ee356389a3
xtaci <xtaci@ubuntu.(none)>
2013-04-18 02:37:54 +0800

    new command: newcity city_name

--- src/db/city/city.go
+++ src/db/city/city.go
@@ -3,11 +3,13 @@ package city
 import . "db"
 import . "types"
 import "strings"
+import "fmt"
 
 func Flush(city *City) {
 	fields, values := SQL_dump(city)
-	stmt := []string{"REPLACE INTO cities(", strings.Join(fields, ","),
-		") VALUES (", strings.Join(values, ","), ")"}
+	changes := SQL_set_clause(fields,values)
+
+	stmt := []string{"UPDATE cities SET ", strings.Join(changes, ","), " WHERE id=", fmt.Sprint(city.Id)}
 
 	db := <-DBCH
 	defer func(){DBCH <- db}()
