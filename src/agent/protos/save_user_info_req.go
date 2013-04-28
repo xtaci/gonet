ae56c951639995ab0b29e5c364b12d8fed85f2d5
xtaci <xtaci@ubuntu.(none)>
2013-04-28 17:07:27 +0800

    implement save_user_info_req

--- src/agent/protos/save_user_info_req.go
+++ src/agent/protos/save_user_info_req.go
@@ -1,8 +1,14 @@
 package protos
 
-import . "types"
-import "misc/packet"
+import (
+	. "types"
+	"misc/packet"
+	"db/user_tbl"
+)
 
 func _save_user_info_req(sess *Session, reader *packet.Packet) (ret []byte, err error) {
+	tbl,_ := pktread_user_archives_info(reader)
+	sess.User.Archives = tbl.F_archives
+	user_tbl.Flush(&sess.User)
 	return
 }
