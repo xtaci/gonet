cdd1addc2bbc2853ec5bf606d2fb95407f5c987a
xtaci <xtaci@ubuntu.(none)>
2013-05-03 14:25:14 +0800

    add a comment for user.go

--- src/types/user.go
+++ src/types/user.go
@@ -11,6 +11,10 @@ const (
 	PROTECTED
 )
 
+/*
+ * User is Stored in both ranklist & session.User for efficiency
+ * MAKE SURE UPDATE BOTH RankList & Session
+ */
 type User struct {
 	Id           int32
 	Name         string
