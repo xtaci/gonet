6e271a8af01cc25f3aed3c5275e513cc89ac61e4
xtaci <daniel820313@gmail.com>
2013-05-19 11:40:10 +0800

    generic timer

--- src/misc/timer/timer.go
+++ src/misc/timer/timer.go
@@ -2,14 +2,13 @@ package production
 
 import (
 	"sync/atomic"
+	"sync"
 	"time"
 )
 
-type Item struct {
-	Id uint32
+type Event struct {
 	User int32
-	Expire int32
-	Finish int64
+	Timeout int64
 }
 
 const (
@@ -18,13 +17,21 @@ const (
 
 var (
 	_incr uint32
-	_eventlist [TIMER_LEVEL]map[uint32]*Item
+	_eventlist [TIMER_LEVEL]map[uint32]*Event
+
+	_eventqueue map[uint32]*Event
+	_eventqueue_lock sync.Mutex
+
+	_cancelqueue []uint32
+	_cancelqueue_lock sync.Mutex
 )
 
 func init() {
 	for k := range _eventlist {
-		_eventlist[k] = make(map[uint32]*Item)
+		_eventlist[k] = make(map[uint32]*Event)
 	}
+
+	_eventqueue = make(map[uint32]*Event)
 }
 
 func TimerRoutine() {
@@ -34,14 +41,42 @@ func TimerRoutine() {
 		time.Sleep(time.Second)
 		timer_count++
 
-		_trigger(0)
+		now := time.Now().Unix()
+		// add pending events
+		_eventqueue_lock.Lock()
+		for k,v := range _eventqueue {
+			diff := v.Timeout - now
+			if diff <= 0 {	// in case of very near event
+				diff = 1
+			}
 
-		for i:=uint(1);i<TIMER_LEVEL;i++ {
+			for i:= TIMER_LEVEL-1;i>=0;i-- {
+				if diff >= 1 << i {
+					_eventlist[i][k] = v
+					break
+				}
+			}
+		}
+		_eventqueue = make(map[uint32]*Event)
+		_eventqueue_lock.Unlock()
+
+		// cancelqueue
+		for _,v := range _cancelqueue {
+			for i:= TIMER_LEVEL-1;i>=0;i-- {
+				list := _eventlist[i]
+				delete(list,v)
+			}
+		}
+
+		// triggers
+		for i:= TIMER_LEVEL-1;i>0;i-- {
 			mask := (uint32(1) << i)-1
 			if timer_count & mask == 0 {
 				_trigger(i)
 			}
 		}
+
+		_trigger(0)
 	}
 }
 
@@ -50,24 +85,30 @@ func _trigger(level uint) {
 	list := _eventlist[level]
 
 	for k,v := range list {
-		if v.Finish - now <= 1 << level {
+		if v.Timeout - now < 1 << level {
 			// move to one closer timer or just removal
 			if level == 0 {
-				// trigger event
 			} else {
+				_eventlist[level-1][k] = v
 				delete(list, k)
 			}
 		}
 	}
 }
 
-//----------------------------------------------- add a timeout for user, 'finish' is unix time
-func Add(id int32, finish int64) uint32 {
-	//expire:= int32(finish - time.Now().Unix())
-	timer_id := atomic.AddUint32(&_incr, 1)
+//----------------------------------------------- add a timeout for user
+func Add(user_id int32, timeout int64) uint32 {
+	event_id:= atomic.AddUint32(&_incr, 1)
+	event := &Event{User:user_id, Timeout:timeout}
 
-	return timer_id
+	_eventqueue_lock.Lock()
+	_eventqueue[event_id] = event
+	_eventqueue_lock.Unlock()
+	return event_id
 }
 
-func Cancel(id int32, product int32) {
+func Cancel(event_id uint32) {
+	_cancelqueue_lock.Lock()
+	_cancelqueue = append(_cancelqueue, event_id)
+	_cancelqueue_lock.Unlock()
 }
