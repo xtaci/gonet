4e05b81126b67ddb523c19c6c97509b9f49321f9
xtaci <xtaci@ubuntu.(none)>
2013-05-20 14:23:27 +0800

    change Init to NewCtx

--- src/misc/crypto/pike/pike_test.go
+++ src/misc/crypto/pike/pike_test.go
@@ -4,14 +4,13 @@ import "testing"
 import "fmt"
 
 func TestPike(t *testing.T) {
-	ctx := &FF_CTX{}
-	Init(0, ctx)
+	ctx := NewCtx(0)
 	fmt.Println("###")
 	fmt.Println(ctx.sd)
 	fmt.Println(ctx.addikey[0].buffer)
 	fmt.Println(ctx.addikey[1].buffer)
 	fmt.Println(ctx.addikey[2].buffer)
-	data := []byte{1,2,3,4,5,6,7,8}
+	data := []byte{1, 2, 3, 4, 5, 6, 7, 8}
 	Encode(ctx, data)
 	fmt.Println(data)
 }
