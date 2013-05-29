734f4d439934898d1be3d2e22a229f9d84d61af9
xtaci <xtaci@ubuntu.(none)>
2013-05-29 16:40:45 +0800

    modify test case

--- src/misc/alg/gaussian/gaussian.go
+++ src/misc/alg/gaussian/gaussian.go
@@ -3,22 +3,22 @@ package gaussian
 import "math"
 
 const (
-	SQRT2PI = float64(2.506628274631001)
-	SIGMA = float64(1.0)
-	MU = float64(0.0)
+	SQRT2PI     = float64(2.506628274631001)
+	SIGMA       = float64(1.0)
+	MU          = float64(0.0)
 	MAX_SAMPLES = 128
 )
 
 type Dist struct {
-	samples [MAX_SAMPLES]int64
-	ptr int
-	n	int
-	sigma float64
+	samples [MAX_SAMPLES]int16
+	ptr     int
+	n       int
+	sigma   float64
 }
 
-func (dist *Dist) Add(x int64) {
+func (dist *Dist) Add(x int16) {
 	dist.samples[dist.ptr] = x
-	if dist.ptr++;dist.ptr >= MAX_SAMPLES {
+	if dist.ptr++; dist.ptr >= MAX_SAMPLES {
 		dist.ptr = 0
 	}
 
@@ -28,26 +28,26 @@ func (dist *Dist) Add(x int64) {
 
 	// caculate mean
 	sum := int64(0)
-	for i:=0;i<dist.n;i++ {
-		sum += dist.samples[i]
+	for i := 0; i < dist.n; i++ {
+		sum += int64(dist.samples[i])
 	}
 
-	mean := float64(sum)/float64(dist.n)
+	mean := float64(sum) / float64(dist.n)
 
-	// caculate standard deviation 
+	// caculate standard deviation
 	sum2 := float64(0.0)
-	for i:=0;i<dist.n;i++ {
+	for i := 0; i < dist.n; i++ {
 		v := float64(dist.samples[i]) - mean
-		v = v*v
+		v = v * v
 		sum2 += v
 	}
 
-	dist.sigma = math.Sqrt(sum2/float64(dist.n))
+	dist.sigma = math.Sqrt(sum2 / float64(dist.n))
 }
 
-func (dist *Dist) P(x int64) float64 {
+func (dist *Dist) P(x int16) float64 {
 	X := float64(x)
-	A := 1.0/(dist.sigma * SQRT2PI)
-	B := math.Exp(-(X*X)/(2*dist.sigma*dist.sigma))
-	return A*B
+	A := 1.0 / (dist.sigma * SQRT2PI)
+	B := math.Exp(-(X * X) / (2 * dist.sigma * dist.sigma))
+	return A * B
 }
--- src/misc/alg/gaussian/gaussian_test.go
+++ src/misc/alg/gaussian/gaussian_test.go
@@ -1,9 +1,9 @@
 package gaussian
 
 import (
-	"testing"
 	"fmt"
 	"math/rand"
+	"testing"
 	"time"
 )
 
@@ -13,28 +13,24 @@ func TestGauss(t *testing.T) {
 
 	// gaussian
 	gaussian := Dist{}
-	for i:=0;i<1000;i++ {
-		v := gen.Int63n(10)
+	for i := 0; i < 1000; i++ {
+		v := int16(gen.Int31n(10))
 		gaussian.Add(v)
 	}
 
 	fmt.Println("N:", gaussian.n, "SIGMA:", gaussian.sigma)
 	fmt.Println("Samples:", gaussian.samples)
-	sum := int64(0)
-	for _,v := range gaussian.samples {
-		sum+=v
-	}
 
 	// testing
 	fmt.Println("range [0,10]")
-	for i:=0;i<10;i++ {
-		v := gen.Int63n(10)
+	for i := 0; i < 10; i++ {
+		v := int16(gen.Int31n(10))
 		fmt.Println(v, ":", gaussian.P(v))
 	}
 
 	fmt.Println("range [0,20]")
-	for i:=0;i<10;i++ {
-		v := gen.Int63n(20)
+	for i := 0; i < 10; i++ {
+		v := int16(gen.Int31n(20))
 		fmt.Println(v, ":", gaussian.P(v))
 	}
 }
