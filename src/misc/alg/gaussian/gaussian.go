beea50abdec0312e7aea526f2c55c00b325025c0
xtaci <xtaci@ubuntu.(none)>
2013-05-29 16:24:17 +0800

    complete the gaussian function, add test

--- src/misc/alg/gaussian/gaussian.go
+++ src/misc/alg/gaussian/gaussian.go
@@ -6,15 +6,48 @@ const (
 	SQRT2PI = float64(2.506628274631001)
 	SIGMA = float64(1.0)
 	MU = float64(0.0)
+	MAX_SAMPLES = 128
 )
 
-//------------------------------------------------ Standard Deviation
-func StdDeviation(x float64) float64 {
-	return P(x, MU, SIGMA)
+type Dist struct {
+	samples [MAX_SAMPLES]int64
+	ptr int
+	n	int
+	sigma float64
 }
 
-func P(x float64, mu float64, sigma float64) float64 {
-	A := 1.0/(sigma * SQRT2PI)
-	B := math.Exp(-((x-mu)*(x-mu))/(2*sigma*sigma))
+func (dist *Dist) Add(x int64) {
+	dist.samples[dist.ptr] = x
+	if dist.ptr++;dist.ptr >= MAX_SAMPLES {
+		dist.ptr = 0
+	}
+
+	if dist.n < MAX_SAMPLES {
+		dist.n++
+	}
+
+	// caculate mean
+	sum := int64(0)
+	for i:=0;i<dist.n;i++ {
+		sum += dist.samples[i]
+	}
+
+	mean := float64(sum)/float64(dist.n)
+
+	// caculate standard deviation 
+	sum2 := float64(0.0)
+	for i:=0;i<dist.n;i++ {
+		v := float64(dist.samples[i]) - mean
+		v = v*v
+		sum2 += v
+	}
+
+	dist.sigma = math.Sqrt(sum2/float64(dist.n))
+}
+
+func (dist *Dist) P(x int64) float64 {
+	X := float64(x)
+	A := 1.0/(dist.sigma * SQRT2PI)
+	B := math.Exp(-(X*X)/(2*dist.sigma*dist.sigma))
 	return A*B
 }
--- src/misc/alg/gaussian/gaussian_test.go
+++ src/misc/alg/gaussian/gaussian_test.go
@@ -3,10 +3,38 @@ package gaussian
 import (
 	"testing"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func TestGauss(t *testing.T) {
-	for i:=-5.0;i<=5.0;i+=0.1 {
-		fmt.Println(StdDeviation(float64(i)))
+	src := rand.NewSource(time.Now().Unix())
+	gen := rand.New(src)
+
+	// gaussian
+	gaussian := Dist{}
+	for i:=0;i<1000;i++ {
+		v := gen.Int63n(10)
+		gaussian.Add(v)
+	}
+
+	fmt.Println("N:", gaussian.n, "SIGMA:", gaussian.sigma)
+	fmt.Println("Samples:", gaussian.samples)
+	sum := int64(0)
+	for _,v := range gaussian.samples {
+		sum+=v
+	}
+
+	// testing
+	fmt.Println("range [0,10]")
+	for i:=0;i<10;i++ {
+		v := gen.Int63n(10)
+		fmt.Println(v, ":", gaussian.P(v))
+	}
+
+	fmt.Println("range [0,20]")
+	for i:=0;i<10;i++ {
+		v := gen.Int63n(20)
+		fmt.Println(v, ":", gaussian.P(v))
 	}
 }
