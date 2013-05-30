e7919c1abb41065971337b5a48b1139ef1122960
xtaci <xtaci@ubuntu.(none)>
2013-05-30 17:39:27 +0800

    enhance hero structure

---
+++ src/types/heroes/hero.go
@@ -0,0 +1,64 @@
+package hero
+
+import (
+	"fmt"
+	"time"
+)
+
+const (
+	COLLECTION = "HEROES"
+	STATUS_NORMAL = byte(0)
+	STATUS_CD     = 1
+)
+
+//----------------------------------------------- Generic Cooldown event records
+type CD struct {
+	OID     uint32
+	Timeout int64
+}
+
+type Hero struct {
+	TYPE string // type in string
+	Status byte
+	Spec map[string]string
+}
+
+type Manager struct {
+	UserId  int32
+	Version uint32
+	Heroes  map[uint32]*Hero // OID->Hero
+	CDs     map[string]*CD   // waiting CoolDown
+}
+
+func (m *Manager) Append(oid uint32, hero *Hero) {
+	if m.Heroes == nil {
+		m.Heroes = make(map[uint32]*Hero)
+	}
+
+	m.Heroes[oid] = hero
+}
+
+func (m *Manager) AppendCD(event_id uint32, cd *CD) {
+	if m.CDs == nil {
+		m.CDs = make(map[string]*CD)
+	}
+	m.CDs[fmt.Sprint(event_id)] = cd
+}
+
+//------------------------------------------------ return num of changes
+func (m *Manager) CheckCD() int {
+	opcount := 0
+	for k := range m.CDs {
+		if m.CDs[k].Timeout <= time.Now().Unix() { // times up
+			oid := m.CDs[k].OID
+			if hero := m.Heroes[oid]; hero != nil {
+				hero.Status = STATUS_NORMAL
+				opcount++
+				delete(m.Heroes, oid)
+			}
+			delete(m.CDs, k)
+		}
+	}
+
+	return opcount
+}
