a00e54b5ce65bdbd82e6646bdd8d4a459f91da4e
xtaci <xtaci@ubuntu.(none)>
2013-05-31 16:31:05 +0800

    sorry, forgot to add 2 files

---
+++ src/agent/client_protos/common.go
@@ -0,0 +1,15 @@
+package protos
+
+import "log"
+import "runtime"
+
+func checkErr(err error) {
+	if err != nil {
+		funcName, file, line, ok := runtime.Caller(1)
+		if ok {
+			log.Printf("ERR:%v,[func:%v,file:%v,line:%v]\n", err, runtime.FuncForPC(funcName).Name(), file, line)
+		}
+
+		panic("error occured in protocol module")
+	}
+}
