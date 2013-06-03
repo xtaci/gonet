271d7b8a8808805bd3fc722866feaa4e123b49a2
xtaci <daniel820313@gmail.com>
2013-06-03 00:23:03 -0700

    add a benchmark for gamedata

--- src/gamedata/gamedata_test.go
+++ src/gamedata/gamedata_test.go
@@ -14,3 +14,10 @@ func TestParser(t *testing.T) {
 	hash := naming.FNV1a("锻造室")
 	fmt.Println(Query(hash))
 }
+
+func BenchmarkParser(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		GetString("建筑规格", "锻造室", "占用格子数")
+		GetInt("部落大厅等级", "20", "金库")
+	}
+}
