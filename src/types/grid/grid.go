baa017d863aa9ce01214a3609b3feaf0caf4a820
xtaci <daniel820313@gmail.com>
2013-06-06 10:16:01 +0800

    fixed a bug in grid byte*byte overflow, changed to integer, added a test case

--- src/types/grid/grid.go
+++ src/types/grid/grid.go
@@ -1,35 +1,35 @@
 package grid
 
 const (
-	W = byte(50)
+	W = 50
 	H = 50
 )
 
 type Grid struct {
-	M []uint16
+	_m []uint16
 }
 
 //------------------------------------------------ Create a new grid struct
 func New() *Grid {
 	g := &Grid{}
-	g.M = make([]uint16, int(W)*int(H))
+	g._m = make([]uint16, W*H)
 	return g
 }
 
 //------------------------------------------------ Set v->[X,Y]
-func (g *Grid) Set(X, Y byte, v uint16) {
-	if X >= 0 && X <= W {
-		if Y >= 0 && Y <= H {
-			g.M[Y*W+X] = v
+func (g *Grid) Set(X, Y int, v uint16) {
+	if X >= 0 && X < W {
+		if Y >= 0 && Y < H {
+			g._m[Y*W+X] = v
 		}
 	}
 }
 
 //------------------------------------------------ Get <-[X,Y]
-func (g *Grid) Get(X, Y byte) uint16 {
-	if X >= 0 && X <= W {
-		if Y >= 0 && Y <= H {
-			return g.M[Y*W+X]
+func (g *Grid) Get(X, Y int) uint16 {
+	if X >= 0 && X < W {
+		if Y >= 0 && Y < H {
+			return g._m[Y*W+X]
 		}
 	}
 
