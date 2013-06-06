6109616e8a878f7031d943b6daa2d4bdaf8fc3b5
xtaci <daniel820313@gmail.com>
2013-06-06 16:14:08 +0800

    add object types

--- src/stats/core/object.go
+++ src/stats/core/object.go
@@ -20,12 +20,17 @@ const (
 	TYPE_TRAIN_HERO    // 训练完成一个英雄, Value为(兵种:数量）
 	TYPE_HERO_FIGHT    //英雄出战一次
 
-	// 资源
+	// 生产
 	TYPE_PRODUCT_NATURAL // 自然产出
 	TYPE_PRODUCE_PVE     // 一次PVE 资源产出
 	TYPE_PRODUCE_PVP     // 一次PVP资源产出
+
+	// 消耗
 	TYPE_CONSUME_UPGRADE // 一次升级消耗
 	TYPE_CONSUME_TRAIN   // 一次训练消耗
+	TYPE_CONSUME_EQUIP   //  一次强化装备消耗
+	TYPE_CONSUME_SEARCH  //  一次搜寻对手消耗
+	TYPE_LOST_RESOURCE   // 一次资源损失
 
 	// 宝石
 	TYPE_CONSUME_GEM // 一次宝石消耗
@@ -34,7 +39,7 @@ const (
 type StatsObject struct {
 	UserId    int32
 	Type      int32
-	Value     []byte
+	Property  map[string]string
 	Timestamp int64
 	// TODO: add fields
 }
