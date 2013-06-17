7c2152a2f449ebc95d02fc04eff011e9d0c52906
xtaci <daniel820313@gmail.com>
2013-06-17 17:12:22 +0800

    add soldier to session and  make flush func save it .

--- src/agent/flush.go
+++ src/agent/flush.go
@@ -10,12 +10,14 @@ import (
 	. "types"
 	"types/estates"
 	"types/samples"
+	"types/soldiers"
 )
 
 //------------------------------------------------ flush all user data
 func _flush(sess *Session) {
 	user_tbl.Set(sess.User)
 	data_tbl.Set(estates.COLLECTION, &sess.Estates)
+	data_tbl.Set(soldiers.COLLECTION, &sess.Soldiers)
 	data_tbl.Set(samples.COLLECTION, &sess.LatencySamples)
 	sess.LastFlushTime = time.Now().Unix()
 	sess.OpCount = 0
