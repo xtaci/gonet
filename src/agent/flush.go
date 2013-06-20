170d7ac3604605645ebb4ddaa4f3b29fc8896ab3
xtaci <daniel820313@gmail.com>
2013-06-20 17:28:26 +0800

    add heroes to flush.go

--- src/agent/flush.go
+++ src/agent/flush.go
@@ -9,6 +9,7 @@ import (
 	"db/user_tbl"
 	. "types"
 	"types/estates"
+	"types/heroes"
 	"types/samples"
 	"types/soldiers"
 )
@@ -18,6 +19,7 @@ func _flush(sess *Session) {
 	user_tbl.Set(sess.User)
 	data_tbl.Set(estates.COLLECTION, &sess.Estates)
 	data_tbl.Set(soldiers.COLLECTION, &sess.Soldiers)
+	data_tbl.Set(heroes.COLLECTION, &sess.Heroes)
 	data_tbl.Set(samples.COLLECTION, &sess.LatencySamples)
 	sess.LastFlushTime = time.Now().Unix()
 	sess.OpCount = 0
