ce8a54937497c3fdf0f81ed1d1f6a66e878fda18
xtaci <daniel820313@gmail.com>
2013-06-20 10:39:36 +0800

    add a benchmark for geoip

--- src/misc/geoip/geoip_test.go
+++ src/misc/geoip/geoip_test.go
@@ -13,3 +13,11 @@ func TestGeoIP(t *testing.T) {
 		t.Error("ip mismatch")
 	}
 }
+
+func BenchmarkGeoIP(b *testing.B) {
+	ip := net.ParseIP("103.14.100.100")
+
+	for i := 0; i < b.N; i++ {
+		Query(ip)
+	}
+}
