7f7851883055bf1985127a6b9267934a46177224
xtaci <daniel820313@gmail.com>
2013-06-21 13:42:48 +0800

    repush un-processed ipcobject back to db

--- src/agent/close_work.go
+++ src/agent/close_work.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"log"
+)
+
+import (
 	"agent/ipc"
+	"db/forward_tbl"
 	. "types"
 )
 
@@ -11,6 +16,17 @@ func close_work(sess *Session) {
 		ipc.Logout(sess.User.Id)
 		ipc.UnregisterOnline(sess.User.Id)
 		close(sess.MQ)
+
+		// 未处理的IPC数据，重新放入db
+		if len(sess.MQ) > 0 {
+			log.Println("re-push ipcobject back to db")
+		}
+
+		for len(sess.MQ) > 0 {
+			ipcobject := <-sess.MQ
+			ipcobject.MarkDelete = false
+			forward_tbl.Push(&ipcobject)
+		}
 
 		// 持久化逻辑#3: 离线时，刷入数据库
 		_flush(sess)
