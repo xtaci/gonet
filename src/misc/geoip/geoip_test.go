b20595de0401feb58a9f5eab2038b316d80a1a6b
xtaci <daniel820313@gmail.com>
2014-03-05 14:27:43 +0800

    更新geoip库，简化geoip的读取

--- src/misc/geoip/geoip_test.go
+++ src/misc/geoip/geoip_test.go
@@ -7,11 +7,13 @@ import (
 
 func TestGeoIP(t *testing.T) {
 	ip := net.ParseIP("103.14.100.100")
-
 	t.Log(Query(ip))
-	if Query(ip) != "HK" {
-		t.Error("ip mismatch")
-	}
+	ip = net.ParseIP("171.216.90.18")
+	t.Log(Query(ip))
+	ip = net.ParseIP("8.35.201.33")
+	t.Log(Query(ip))
+	ip = net.ParseIP("62.216.125.241")
+	t.Log(Query(ip))
 }
 
 func BenchmarkGeoIP(b *testing.B) {
