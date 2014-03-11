192762264658f8f39782adf5c3d83a1db57cd0af
xtaci <daniel820313@gmail.com>
2014-03-11 18:21:45 +0800

    优化timer的实现

--- src/misc/timer/timer_test.go
+++ src/misc/timer/timer_test.go
@@ -8,19 +8,16 @@ func TestTimer(t *testing.T) {
 	ch := make(chan int32, 100)
 
 	now := time.Now().Unix()
-	Add(1, now+1, ch)
-	Add(2, now+2, ch)
-	Add(3, now+3, ch)
-	Add(4, now+4, ch)
-	Add(5, now+5, ch)
-	// Add(10, now+10, ch)
-	//	Add(now+60, ch)
+
+	for i := 0; i < 100; i++ {
+		Add(1, now+int64(i), ch)
+	}
 
 	count := 0
 	for {
 		fmt.Println("timer event :", <-ch)
 		count++
-		if count == 5 {
+		if count == 100 {
 			break
 		}
 	}
