4635bc8660f471c3445eeae7c445a01fc2c47570
xtaci <daniel820313@gmail.com>
2014-04-09 20:03:40 +0800

    remove obsolete code

--- src/db/stats_tbl/stats.go
+++ src/db/stats_tbl/stats.go
@@ -2,7 +2,6 @@ package stats_tbl
 
 import (
 	"cfg"
-	"fmt"
 	"labix.org/v2/mgo"
 	"labix.org/v2/mgo/bson"
 	"os"
@@ -67,62 +66,4 @@ func SetUpdate(key, value, lang string) bool {
 		ERR(STR_GAME_INFO, "SetUpdate", err, key, value, lang)
 	}
 	return true
-}
-
-//------------------------------------------------------------ 得到玩家日志
-func GetPlayLog(userid int, key string, start, end int) []IntGameInfo {
-	ms, c := C(INT_GAME_INFO)
-	defer ms.Close()
-	outs := make([]IntGameInfo, 0)
-	k := fmt.Sprintf("%v#%v", userid, key)
-	start_time := time.Unix(int64(start), 0).UTC()
-	end_time := time.Unix(int64(end), 0).UTC()
-	err := c.Find(bson.M{"$and": []bson.M{bson.M{"time": bson.M{"$gte": start_time}}, bson.M{"time": bson.M{"$lt": end_time}}, bson.M{"key": k}}}).All(&outs)
-	if err != nil {
-		ERR(INT_GAME_INFO, "GetPlayLog", err, userid, key, start, end)
-		return nil
-	}
-	return outs
-}
-
-//--------------------------------------------------------- 得到gacha日志
-func GetGachaLog(userid, start, end int) []IntGameInfo {
-	ms, c := C(INT_GAME_INFO)
-	defer ms.Close()
-	outs := make([]IntGameInfo, 0)
-	k := fmt.Sprintf("^%v#gacha#", userid)
-	start_time := time.Unix(int64(start), 0).UTC()
-	end_time := time.Unix(int64(end), 0).UTC()
-	err := c.Find(bson.M{"$and": []bson.M{bson.M{"time": bson.M{"$gte": start_time}}, bson.M{"time": bson.M{"$lt": end_time}}, bson.M{"key": bson.M{"$regex": k, "$options": "i"}}}}).All(&outs)
-	if err != nil {
-		ERR(INT_GAME_INFO, "GetGachaLog", err, userid, start, end)
-		return nil
-	}
-	return outs
-}
-
-//--------------------------------------------------------- 得到某段时间的adds信息
-func GetAdds(start, end int64) []IntGameInfo {
-	ms, c := C(INT_GAME_INFO)
-	defer ms.Close()
-	outs := make([]IntGameInfo, 0)
-	err := c.Find(bson.M{"$and": []bson.M{bson.M{"time": bson.M{"$gte": time.Unix(start, 0)}}, bson.M{"time": bson.M{"$lt": time.Unix(end, 0)}}}}).All(&outs)
-	if err != nil {
-		ERR(INT_GAME_INFO, "GetAdds", err, start, end)
-		return nil
-	}
-	return outs
-}
-
-//--------------------------------------------------------- 得到某段时间update信息
-func GetStatus(start, end int64) []StrGameInfo {
-	ms, c := C(STR_GAME_INFO)
-	defer ms.Close()
-	outs := make([]StrGameInfo, 0)
-	err := c.Find(bson.M{"$and": []bson.M{bson.M{"time": bson.M{"$gte": time.Unix(start, 0)}}, bson.M{"time": bson.M{"$lt": time.Unix(end, 0)}}}}).All(&outs)
-	if err != nil {
-		ERR(STR_GAME_INFO, "GetStatus", err, start, end)
-		return nil
-	}
-	return outs
 }
