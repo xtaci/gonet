0738fd3523192a2ee93f1a0cb0d2b94a2bc1b965
xtaci <daniel820313@gmail.com>
2014-04-29 11:12:07 +0800

    using the method from 2014 gophercon -- <<A Channel Compendium >>for closing multiple goroutines

--- src/agent/signal.go
+++ src/agent/signal.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"sync/atomic"
 	"syscall"
 )
 
@@ -27,7 +26,7 @@ func SignalProc() {
 			cfg.Reload()
 			gamedata.Reload()
 		case syscall.SIGTERM: // server close
-			atomic.StoreInt32(&SIGTERM, 1)
+			close(die)
 			log.Println("\033[043;1m[SIGTERM]\033[0m")
 			INFO("waiting for agents close, please wait...")
 			wg.Wait()
