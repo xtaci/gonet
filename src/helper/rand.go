c1ab26be450b56432a70a21c89ae88b12d5c1276
xtaci <daniel820313@gmail.com>
2014-05-20 11:08:37 +0800

    optimize rand number generator

--- src/helper/rand.go
+++ src/helper/rand.go
@@ -1,21 +1,22 @@
 package helper
 
 import (
-	"sync/atomic"
 	"time"
 )
 
-var _X0 uint32 = uint32(time.Now().UnixNano())
-var _a uint32 = 1664525
-var _c uint32 = 1013904223
+var x0 uint32 = uint32(time.Now().UnixNano())
+var a uint32 = 1664525
+var c uint32 = 1013904223
 
-//------------------------------------------------ Linear congruential generator, very fast!!
-func LCG() uint32 {
-	for {
-		X0 := atomic.LoadUint32(&_X0)
-		Xn := _a*X0 + _c
-		if atomic.CompareAndSwapUint32(&_X0, X0, Xn) {
-			return Xn
+var LCG chan uint32
+
+//------------------------------------------------ 全局快速随机数发生器
+func init() {
+	LCG = make(chan uint32)
+	go func() {
+		for {
+			x0 = a*x0 + c
+			LCG <- x0
 		}
-	}
+	}()
 }
