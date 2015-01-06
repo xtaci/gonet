38ac378b48d63239f654621a1e92f86775873c41
fuli <fuli@nibirutech.com>
2015-01-06 15:39:23 +0800

    optimize timer implementation

--- src/misc/timer/timer.go
+++ src/misc/timer/timer.go
@@ -1,9 +1,13 @@
 package timer
 
 import (
-	"sync"
-	"sync/atomic"
+	"log"
 	"time"
+)
+
+const (
+	TIMER_LEVEL = 20 // 时间段最大分级，最大时间段为 2^TIMERLEVEL
+	QUEUE_MAX   = 65536
 )
 
 type _timer_event struct {
@@ -12,17 +16,9 @@ type _timer_event struct {
 	CH      chan int32 // 发送通道
 }
 
-const (
-	TIMER_LEVEL = uint(16) // 时间段最大分级，最大时间段为 2^TIMERLEVEL
-)
-
 var (
-	_eventlist [TIMER_LEVEL]map[uint32]_timer_event // 事件列表
-
-	_eventqueue      map[uint32]_timer_event // 事件添加队列
-	_eventqueue_lock sync.Mutex
-
-	_timer_id uint32 // 内部事件编号
+	_eventlist  [TIMER_LEVEL]map[uint32]_timer_event // 事件列表
+	_eventqueue chan _timer_event                    // 事件添加队列
 )
 
 func init() {
@@ -30,7 +26,7 @@ func init() {
 		_eventlist[k] = make(map[uint32]_timer_event)
 	}
 
-	_eventqueue = make(map[uint32]_timer_event)
+	_eventqueue = make(chan _timer_event, QUEUE_MAX)
 
 	go _timer()
 }
@@ -39,79 +35,77 @@ func init() {
 // 定时器 goroutine
 // 根据程序启动后经过的秒数计数
 func _timer() {
-	timer_count := uint32(0)
+	defer func() {
+		if x := recover(); x != nil {
+			log.Println("TIMER CRASHED", x)
+		}
+	}()
+
 	last := time.Now().Unix()
+	timer_id := uint32(0) // 内部事件编号
 
+	sleep_timer := time.NewTimer(time.Second)
 	for {
-		time.Sleep(100 * time.Millisecond)
-
-		// 处理排队
-		// 最小的时间间隔，处理为1s
-		_eventqueue_lock.Lock()
-		for k, v := range _eventqueue {
-			// 处理微小间隔
-			diff := v.Timeout - time.Now().Unix()
+		select {
+		case new_event := <-_eventqueue:
+			timer_id++
+			// 最小的时间间隔，处理为1s
+			diff := new_event.Timeout - time.Now().Unix()
 			if diff <= 0 {
 				diff = 1
 			}
 
 			// 发到合适的框
 			for i := TIMER_LEVEL - 1; i >= 0; i-- {
-				if diff >= 1<<i {
-					_eventlist[i][k] = v
+				if diff >= 1<<uint(i) {
+					_eventlist[i][timer_id] = new_event
 					break
 				}
 			}
+		case <-sleep_timer.C:
+			// 重置定时器
+			sleep_timer.Reset(time.Second)
 
-			// 从队列中删除这个事件
-			delete(_eventqueue, k)
-		}
-		_eventqueue_lock.Unlock()
-
-		// 检查事件触发
-		// 累计距离上一次触发的秒数,并逐秒触发
-		// 如果校正了系统时间，时间前移，nsec为负数的时候，last的值不应该变动，否则会出现秒数的重复计数
-		now := time.Now().Unix()
-		nsecs := now - last
-
-		if nsecs <= 0 {
-			continue
-		} else {
-			last = now
-		}
-
-		for c := int64(0); c < nsecs; c++ {
-			timer_count++
-
-			for i := TIMER_LEVEL - 1; i > 0; i-- {
-				mask := (uint32(1) << i) - 1
-				if timer_count&mask == 0 {
-					_trigger(i)
-				}
+			// 检查事件触发
+			// 累计距离上一次触发的秒数,并逐秒触发
+			// 如果校正了系统时间，时间前移，nsec为负数的时候，last的值不应该变动，否则会出现秒数的重复计数
+			now := time.Now().Unix()
+			if now-last < 1 {
+				continue
 			}
 
-			_trigger(0)
+			// 开始逐秒触发
+			for {
+				last++
+				for i := TIMER_LEVEL - 1; i >= 0; i-- {
+					mask := (1 << uint(i)) - 1
+					if last&int64(mask) == 0 {
+						_trigger(i)
+					}
+				}
+
+				// 如果到达当前时间，停止循环
+				if last == now {
+					break
+				}
+			}
 		}
 	}
 }
 
 //------------------------------------------------ 单级触发
-func _trigger(level uint) {
+func _trigger(level int) {
 	now := time.Now().Unix()
 	list := _eventlist[level]
 
 	for k, v := range list {
-		if v.Timeout-now < 1<<level {
-			// 移动到前一个更短间距的LIST
-			if level == 0 {
-				func() {
-					defer func() {
-						recover() // ignore closed channel
-					}()
-
-					v.CH <- v.Id
-				}()
-			} else {
+		if v.Timeout-now < 1<<uint(level) {
+			if level == 0 { // 触发事件
+				select { // 发送必须为非阻塞, 传入的chan不能关闭
+				case v.CH <- v.Id:
+				default:
+				}
+			} else { // 移动到前一个更短间距的时间队列
 				_eventlist[level-1][k] = v
 			}
 
@@ -124,10 +118,5 @@ func _trigger(level uint) {
 // 添加一个定时，timeout为到期的Unix时间
 // id 是调用者定义的编号, 事件发生时，会把id发送到ch
 func Add(id int32, timeout int64, ch chan int32) {
-	event := _timer_event{Id: id, CH: ch, Timeout: timeout}
-
-	timer_id := atomic.AddUint32(&_timer_id, 1)
-	_eventqueue_lock.Lock()
-	_eventqueue[timer_id] = event
-	_eventqueue_lock.Unlock()
+	_eventqueue <- _timer_event{Id: id, CH: ch, Timeout: timeout}
 }
--- src/misc/timer/timer_test.go
+++ src/misc/timer/timer_test.go
@@ -5,20 +5,28 @@ import "time"
 import "fmt"
 
 func TestTimer(t *testing.T) {
-	ch := make(chan int32, 100)
+	ch := make(chan int32, 10)
 
 	now := time.Now().Unix()
 
-	for i := 0; i < 100; i++ {
-		Add(1, now+int64(i), ch)
+	for i := 0; i < 10; i++ {
+		Add(int32(i), now+int64(i), ch)
 	}
 
 	count := 0
 	for {
 		fmt.Println("timer event :", <-ch)
 		count++
-		if count == 100 {
+		if count == 10 {
 			break
 		}
+	}
+}
+
+func BenchmarkTimer(b *testing.B) {
+	ch := make(chan int32, 100)
+	now := time.Now().Unix()
+	for i := 0; i < b.N; i++ {
+		Add(1, now+100000, ch)
 	}
 }
