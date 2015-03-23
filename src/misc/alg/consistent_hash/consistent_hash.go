2927abcfa7fa78669252176e7ed3cc82b873f843
xtaci <daniel820313@gmail.com>
2015-03-23 18:13:07 +0800

    add consistent hashing

---
+++ src/misc/alg/consistent_hash/consistent_hash.go
@@ -0,0 +1,80 @@
+package consistent_hash
+
+import (
+	"sync"
+)
+
+type ConsistentHashing struct {
+	split_points []uint32
+	keys         map[uint32]string
+	sync.Mutex
+}
+
+func (ch *ConsistentHashing) Init() {
+	ch.keys = make(map[uint32]string)
+}
+
+//----------------------------------------------- add a keyd hashcode to the circle
+func (ch *ConsistentHashing) AddNode(key string, hashcode uint32) bool {
+	ch.Lock()
+	defer ch.Unlock()
+
+	if _, ok := ch.keys[hashcode]; ok { // test existence
+		return false
+	}
+	ch.keys[hashcode] = key
+
+	// empty
+	if len(ch.split_points) == 0 {
+		ch.split_points = []uint32{hashcode}
+		return true
+	}
+
+	// hashcode in the middle of the circle
+	for i := 0; i < len(ch.split_points); i++ {
+		if hashcode < ch.split_points[i] {
+			ch.split_points = append(ch.split_points[:i], append([]uint32{hashcode}, ch.split_points[i:]...)...)
+			return true
+		}
+	}
+
+	// largest hashcode
+	ch.split_points = append(ch.split_points, hashcode)
+	return true
+}
+
+//----------------------------------------------- remove a node from the circle
+func (ch *ConsistentHashing) RemoveNode(hashcode uint32) bool {
+	ch.Lock()
+	defer ch.Unlock()
+
+	if _, ok := ch.keys[hashcode]; ok {
+		delete(ch.keys, hashcode)
+		for i := 0; i < len(ch.split_points); i++ {
+			if ch.split_points[i] == hashcode {
+				ch.split_points = append(ch.split_points[:i], ch.split_points[i+1:]...)
+				return true
+			}
+		}
+	}
+	return false
+}
+
+//----------------------------------------------- get the node by a given hashcode
+func (ch *ConsistentHashing) GetNode(hashcode uint32) (key string, ok bool) {
+	ch.Lock()
+	defer ch.Unlock()
+
+	if len(ch.split_points) == 0 {
+		return "", false
+	}
+
+	for i := range ch.split_points {
+		if ch.split_points[i] >= hashcode {
+			return ch.keys[ch.split_points[i]], true
+		}
+	}
+
+	// hashcode larger than the largest node, return the first one
+	return ch.keys[ch.split_points[0]], true
+}
